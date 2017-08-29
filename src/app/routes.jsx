a3aaca5262919d596f81f50e95394ccca38a7e3b
Alex Konovalov <akonovalov@at-consulting.ru>
2017-08-29 11:23:07 +0300

    rename

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import About from './components/About/about';
-import Home from './components/Home/home';
+import About from './components/About';
+import Home from './components/Home';
 
 
 const Routes = () => (
