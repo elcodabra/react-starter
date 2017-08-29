19b1d597bf63025bd3c0447c15e2c917af801338
Alex Konovalov <akonovalov@at-consulting.ru>
2017-08-29 12:27:37 +0300

    add page not found

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -3,12 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import About from './components/About';
 import Home from './components/Home';
-
+import PageNotFound from './components/PageNotFound';
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={Home}/>
     <Route exact path='/about' component={About}/>
+    <Route component={PageNotFound}/>
   </Switch>
 )
 
