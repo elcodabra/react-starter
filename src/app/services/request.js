d194f0f663e58c35bac2c5969cf37930f94f4f84
akonovalov <akonovalov@at-consulting.ru>
2017-09-23 15:10:33 +0300

    async/await requests

--- src/app/services/request.js
+++ src/app/services/request.js
@@ -4,27 +4,35 @@ import BackendService from './backend';
  class ApiRequest {
 
    get(url, config) {
-     return this.request(axios.get(url, config))
+     return this.request({
+       method: 'get',
+       url,
+       ...config
+     })
    }
 
    post(url, data, config) {
-     return this.request(axios.post(url, data, config))
+     return this.request({
+       method: 'post',
+       url,
+       data,
+       ...config
+     })
    }
 
-   request(promise) {
-     return new Promise((resolve, reject) => {
-       promise
-         .then((data) => {
-           resolve(data)
-         })
-         .catch((error) => {
-           if (error.response && error.response.status && error.response.status == 404) {
-             document.location.href = BackendService.getUrl()
-           } else {
-             reject(error)
-           }
-         })
-     })
+   async request(config) {
+     try {
+       console.log('resolve 1');
+       return await axios({
+         ...config
+       })
+     } catch (error) {
+       if (error.response && error.response.status && error.response.status == 404) {
+         document.location.href = BackendService.getUrl()
+       } else {
+         return Promise.reject(error)
+       }
+     }
    }
 
  }
